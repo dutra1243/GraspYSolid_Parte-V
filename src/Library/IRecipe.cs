d2dc2eb450ba4039824eae91c57fc558338d9605
dutra1243 <facundodutraextra@gmail.com>
2023-10-26 18:03:53 -0300

    DIP fix between Recipe and ConsolePrinter classes

---
+++ src/Library/IRecipe.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID
+{
+    public interface IRecipe
+    {
+
+        void AddStep(Product input, double quantity, Equipment equipment, int time);
+
+        void AddStep(string description, int time);
+
+        void RemoveStep(BaseStep step);
+
+        string GetTextToPrint();
+
+        double GetProductionCost();
+
+    }
+}
